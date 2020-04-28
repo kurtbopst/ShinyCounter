aeaa87e44e2a917bd4bcd84df89bc39ebc5c8ff0
Kurt Bopst <kurtbopst@gmail.com>
2020-04-27 22:02:57 -0400

    Add project files.

---
+++ ShinyCounter/Data/ProfileContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShinyCounter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShinyCounter.Data
+{
+    public class ProfileContext : DbContext
+    {
+        public ProfileContext (DbContextOptions<ProfileContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Profile> Profile { get; set; } // Qualifies a row within the database table as a PROFILE
+
+    }
+}
---
+++ ShinyCounter/Models/Profile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShinyCounter.Models
+{
+    public class Profile
+    {
+
+
+        public int Id { get; set; }
+
+        //Biograhical Info
+        public string username { get; set; }
+        public string password { get; set; }
+
+
+
+        public bool[] caught { get; set; }
+
+        public int goal { get; set; }
+        public int counter { get; set; }
+
+    }
+}
